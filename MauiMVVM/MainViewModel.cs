4565dd7cd256cc8ed340ab980038a8e5271ef0a1
Marcin Knapik <marcin.knapik@ex7.pl>
2024-05-31 16:39:14 +0200

    Add project files.

---
+++ MauiMVVM/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiMVVM
+{
+    public partial class MainViewModel : ObservableObject // z nugetu CommunityToolkit.Mvvm
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        private string firstName;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        private string lastName;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        private bool isBusy;
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public bool IsNotBusy => !IsBusy;
+
+        [RelayCommand]
+        private void Tap()
+        {
+            IsBusy = true;
+
+            Console.WriteLine(FullName);
+
+            IsBusy = false;
+        }
+    }
+}
